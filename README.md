cb674f70bf0f2c94079a1232c4b0722b941597bc
Renato Santos <renato.santos@modalgr.com.br>
2021-10-04 23:23:42 -0300

    subind tp 01

---
+++ README.md
@@ -0,0 +1,9 @@
+## PDM 
+
+# Tuller
+
+## ALUNOS : 
+
+Renato de Araújo Santos CB3007154
+
+Fernando Avelino da Silva CB3008053
---
+++ TP01/TP01/TP01/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OkCliked()
+        {
+
+            if(lblId.Text == "admin" && lblPass.Text == "senha")
+            {
+                DisplayAlert("Autorizado", "Usuário logado com sucesso", "OK");
+            }
+            else
+            {
+                DisplayAlert("Não autorizado", "ID e/ou Password incorretos", "Tente novamente");
+            }
+
+        }
+
+        public void LimparCliked()
+        {
+            lblId.Text = "";
+            lblPass.Text = "";
+            
+        }
+
+        public void CreditosCliked()
+        {
+            DisplayAlert("Autores: ", "Alexsandro Silva \n Paula Simão", "OK");
+        }
+    }
+}
